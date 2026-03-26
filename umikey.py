ee6f62c485e848b329efbfdc53b16b4b8368bfd4
agavistas <118552874+agavistas@users.noreply.github.com>
2026-03-25 21:39:45 -0400

    initial commit

---
+++ umikey.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from yubikit.yubiotp import YubiOtpSession, SLOT, UpdateConfiguration
+from ykman import scripting as s
+
+try:
+    yubikey = s.single()
+except:
+        print("Please plug in your YubiKey!")
+        exit()
+
+session = YubiOtpSession(yubikey.otp())
+try:
+    serial_number = bytes.fromhex(f"{yubikey.info.serial:#0{12}d}")
+    session.update_configuration(
+        slot=SLOT.ONE,
+        cur_acc_code=serial_number,
+        configuration=UpdateConfiguration(),
+    )
+    print("Access code removed. Enjoy!")
+    exit()
+except:
+    print("Access code was not serial number, this is not a UMich YubiKey!")
+    exit()
